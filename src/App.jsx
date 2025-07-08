cae6e1e978c10e448c75ba9193b9590079020bcd
Lauren Jackson <mslaurenajacksoncybersec109@gmail.com>
2025-07-08 16:03:51 -0400

    Updated App.jsx to add spacebar and question mark

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ function App() {
   // used nested arrays to immulate a keyboard layout
  
   const keyboardRows = [
-                      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,],
             ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
                ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-                    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+                    ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '?']
   ];
 
   const handleDragStart = (letter) => {
@@ -40,6 +40,10 @@ function App() {
       setCommunication([]);
     };
     speechSynthesis.speak(utterance);
+  };
+
+  const handleSpaceBar = () => {
+    setCommunication((prev) => [...prev, ' ']); // Add a space to the communication state
   };
 
   return (
@@ -129,6 +133,22 @@ function App() {
     >
       Speak
     </button>
+
+    <button
+  onClick={handleSpaceBar}
+  style={{
+    marginTop: '20px',
+    padding: '10px 20px',
+    fontSize: '16px',
+    backgroundColor: 'white',
+    color: 'black',
+    border: '1px solid #999',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  }}
+>
+  Space Bar
+</button>
   </div>
 
     </>
