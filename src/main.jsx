a6f6c8630fbd95128a7a4da1a53e333394dcf946
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-07-10 14:31:32 -0500

    Resolve the main.jsx import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from '/Users/laurenjackson/Documents/GitHub/GitHub1/aacDeviceBeta/vite-project/src/App.jsx';
+import App from '/src/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
